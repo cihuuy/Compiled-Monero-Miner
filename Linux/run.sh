60e2054ff0855dcb7c178e8731659dbb84e58171
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-08-18 22:57:28 +0700

    Update run.sh

--- Linux/run.sh
+++ Linux/run.sh
@@ -1,1 +1,1 @@
-./xmrig --cuda -o <Pool> -u <Wallet> -p <Miner-Name> (-k --tls) --cpu-max-threads-hint [25|50|75|100] -o <Another-Pool> -u <Wallet> -p <Miner-Name> (-k --tls)
+./xmrig --cpu-max-threads-hint [25|50|75|100] -o <Another-Pool> -u <Wallet> -p <Miner-Name> (-k --tls)
