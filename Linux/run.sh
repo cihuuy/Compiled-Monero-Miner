16470d6a192a8e6cfb6f979d02d52f2684e838a2
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-08-18 23:00:44 +0700

    Update run.sh

--- Linux/run.sh
+++ Linux/run.sh
@@ -1,1 +1,1 @@
-./xmrig --cpu-max-threads-hint [25|50|75|100] -o <Another-Pool> -u <Wallet> -p <Miner-Name> (-k --tls)
+./xmrig --cpu-max-threads-hint [25|50|75|100] -o pool.hashvault.pro:3333 -u 86P42DaNTvmBmMLM4oL5kL6tVQVo9FfsnJDTqj6VU76whVzjMdMbMa7PV3SHAQuNySan44ToXVFn3gwFmqeDb58t1xqNVAB -p sln (-k --tls)
